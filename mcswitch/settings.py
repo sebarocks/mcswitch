7de9cd67811b42887b3063b0e8ebeb9016cb80d1
Sebastian Orrego <sebastian_orrego_a@outlook.com>
2022-05-12 15:42:47 -0400

    fix static config

--- mcswitch/settings.py
+++ mcswitch/settings.py
@@ -123,8 +123,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = join(BASE_DIR, 'static', 'static_root')
-STATICFILES_DIRS = [join(BASE_DIR,'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
